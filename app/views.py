60683914981b58db29e2fdd63bd8f8584f3d42fb
Mehasawa <ataman@t-human.com>
2024-01-27 13:44:22 +0300

    добавили оформление заказа

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import *
+from .forms import *
 # Create your views here.
 def index(req):
     items = Tovar.objects.all()
@@ -7,12 +8,40 @@ def index(req):
     return render(req,'index.html',data)
 
 def toCart(req):
-    items = Cart.objects.all()
+    items = Cart.objects.filter(user=req.user)
+    forma = OrderForm()
     total=0
     for i in items:
         total += i.calcSumma()
     total = round(total,2)
-    data={'tovari':items, 'total':total}
+    #оформение заказа
+    if req.POST:
+        forma = OrderForm(req.POST)
+        k1 = req.POST.get('adres')
+        k2 = req.POST.get('tel')
+        k3 = req.POST.get('email')
+        print(k1,k2,k3)
+        if forma.is_valid():
+            print(k1, k2, k3)
+            k1=forma.cleaned_data.get('adres')
+            k2=forma.cleaned_data.get('tel')
+            k3=forma.cleaned_data.get('email')
+            print(k1, k2, k3)
+            myzakaz=''
+            for one in items:
+                myzakaz+=one.tovar.name+' '
+                myzakaz+='количество '+str(one.count)+' '
+                myzakaz+='сумма '+str(one.summa)+' '
+                myzakaz+='скидка '+str(one.tovar.discount)+' '
+            Order.objects.create(adres=k1,
+                                 tel=k2,
+                                 email=k3,
+                                 total=total,
+                                 myzakaz=myzakaz,
+                                 user=req.user)
+            items.delete()
+            return render(req,'sps.html')
+    data={'tovari':items, 'total':total, 'formaorder':forma}
     return render(req,'cart.html',data)
 
 def buy(req,id):
